3f5c5a57e79638d362aefbd706a85f2692ad0317
Adam Diament <adam@clevertree.io>
2022-03-07 16:16:36 +0200

    Android binding projects are building

---
+++ src/MobileRTC_Droid/Additions/ZmShareCameraView.cs
@@ -0,0 +1,38 @@
+﻿using System;
+namespace Com.Zipow.Videobox.Confapp.Meeting.Scene.Sharecamera
+{
+    public partial class ZmShareCameraView
+    {
+        void US.Zoom.Androidlib.Widget.ITouchViewControl.Left()
+        {
+           
+            InvokeLeft();
+        }
+        
+        void US.Zoom.Androidlib.Widget.ITouchViewControl.Right()
+        {
+            InvokeRight();
+        }
+    }
+
+ 
+  
+}
+
+
+namespace Com.Zipow.Videobox.View.Ptvideo
+{
+    public partial class ZmPtCameraView
+    {
+        void US.Zoom.Androidlib.Widget.ITouchViewControl.Left()
+        {
+            InvokeLeft();
+        }
+        
+        void US.Zoom.Androidlib.Widget.ITouchViewControl.Right()
+        {
+            InvokeRight();
+        }
+    }
+}
+
