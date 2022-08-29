c4d2260becce9ea3f3e329c5bb4bc9c086d1fa8b
Adam Diament <adam@clevertree.io>
2022-08-29 14:44:09 +0300

    Fix a batch of mobile_rtc errors

--- src/Droid/MobileRTC_Droid/Additions/ZmCameraCharacteristicComparator.cs
+++ src/Droid/MobileRTC_Droid/Additions/ZmCameraCharacteristicComparator.cs
@@ -3,10 +3,10 @@ namespace Com.Zipow.Nydus.Camera
 {
     public partial class ZmCameraCharacteristicComparator
     {
-        public virtual unsafe int Compare(Java.Lang.Object p0, Java.Lang.Object p1)
-        {
-            return Compare((Com.Zipow.Nydus.Camera.ZMCameraCharacteristic) p0, (Com.Zipow.Nydus.Camera.ZMCameraCharacteristic) p1);
-        }
+        //public virtual unsafe int Compare(Java.Lang.Object p0, Java.Lang.Object p1)
+        //{
+        //    return Compare((Com.Zipow.Nydus.Camera.ZMCameraCharacteristic) p0, (Com.Zipow.Nydus.Camera.ZMCameraCharacteristic) p1);
+        //}
     }
 }
 
