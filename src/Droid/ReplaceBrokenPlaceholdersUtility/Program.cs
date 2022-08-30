5ceeeebe6fa835c4e9ebcc07a341bc82720c5a57
Adam Diament <adam@clevertree.io>
2022-08-30 16:44:02 +0300

    Newly fixed common lib

--- src/Droid/ReplaceBrokenPlaceholdersUtility/Program.cs
+++ src/Droid/ReplaceBrokenPlaceholdersUtility/Program.cs
@@ -5,12 +5,34 @@ using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
 
+var regex = new Regex("%(s|d)|%([0-9]+d)|%([0-9]+s)");
+
 Console.WriteLine("Started");
 
 Main();
+//var theLine = "%1s, %2s of %3s, selected";
+//ProcessLine(ref theLine);
 
+//Console.WriteLine(theLine);
 Console.WriteLine("Finished");
-Console.ReadLine();
+
+void ProcessLine(ref string line)
+{
+    var matches = regex.Matches(line);
+    if (matches.Any())
+    {
+        Console.WriteLine($"Found problem in line: {line}");
+        var position = 1;
+        while (matches.Any())
+        {
+            var letter = matches[0].Value.Contains("d") ? "d" : "s";
+            line =  regex.Replace(line, $"%{position}${letter}", 1);
+            position++;
+            matches =  regex.Matches(line);
+        }
+        Console.WriteLine($"Corrected line to: {line}");
+    }
+}
 
 // Download the latest zoom sdk
 // Inside the mobile RTC folder, find the file called mobilertc.aar and rename it to mobilertc.zip
@@ -18,13 +40,13 @@ Console.ReadLine();
 // Run this method making sure to point to the res folder inside the extracted folder
 // Recompile the mobilertc.aar file with this command ```jar cvf mobilertc.aar -C theExtractedFolderName/ .```
 // Your mobilertc.aar file will now be suitable to use in the binding project.
-// Do the same with the commonlib.aar file. ```jar cvf ZoomCommonLib.aar -C theExtractedFolderName/ .```
+// Do the same with the commonlib.aar file. ```jar cvf ZoomCommonLib.aar -C "C:\Users\adam_\OneDrive\Business\Visual Service\Zoom Binding\5.11.3.7251\commonlib\/" .```
 void Main()
 {
-    var regex = new Regex("%(s|d)|%([0-9]+d)");
+
     //change your res folder to where you extracted the mobile rtc and then the common lib e.g C:\Users\adam_\OneDrive\Business\Visual Service\Zoom Binding\5.11.3.7251\commonlib\res
     var resFolder =
-        @"C:\Users\adam_\OneDrive\Business\Visual Service\Zoom Binding\5.11.3.7251\commonlib\res";
+        @"C:\Users\adam_\OneDrive\Business\Visual Service\Zoom Binding\5.11.3.7251\mobilertc\res";
 
     //var resFolder = @"C:\Users\adam_\Downloads\test";
 
@@ -36,20 +58,7 @@ void Main()
         {
             for (var j = 0; j < allLines.Length; j++)
             {
-                var matches = regex.Matches(allLines[j]);
-                if (matches.Any())
-                {
-                    Console.WriteLine($"Found problem in line: {allLines[j]}");
-                    var position = 1;
-                    while (matches.Any())
-                    {
-                        var letter = matches[0].Value.Contains("d") ? "d" : "s";
-                        allLines[j] =  regex.Replace(allLines[j], $"%{position}${letter}", 1);
-                        position++;
-                        matches =  regex.Matches(allLines[j]);
-                    }
-                    Console.WriteLine($"Corrected line to: {allLines[j]}");
-                }
+                ProcessLine(ref allLines[j]);
             }
             File.WriteAllLines(file, allLines);
         }
