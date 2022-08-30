188c67ad149ccac110b8bf681cbf1c6921ae191e
Adam Diament <adam@clevertree.io>
2022-08-30 14:04:54 +0300

    Improve the placeholder replace program

--- src/Droid/ReplaceBrokenPlaceholdersUtility/Program.cs
+++ src/Droid/ReplaceBrokenPlaceholdersUtility/Program.cs
@@ -12,14 +12,33 @@ Main();
 Console.WriteLine("Finished");
 Console.ReadLine();
 
+
+void Test()
+{
+    string toReplace = "Hello %s my name is %d yes %1d yes %2d %d %s %d";
+    var regex = new Regex("%(s|d)|%([0-9]+d)");
+    var matches = regex.Matches(toReplace);
+    var i = 1;
+    Console.WriteLine("Original String: "+toReplace);
+    while (matches.Any())
+    {
+        //var letter = matches[0].Value[1];
+        var letter = "[REPL]";
+        toReplace = regex.Replace(toReplace, $"%{i}${letter}", 1);
+        i++;
+        matches =  regex.Matches(toReplace);
+    }
+    Console.WriteLine("Final String: "+toReplace);
+}
+
 void Main()
 {
-    var placeholders = new []{ "s", "d" };
-
-    //change your res folder to where you extracted the mobile rtc
-    //var resFolder = @"C:\Users\adam_\Downloads\zoom-sdk-android-5.11.3.7251\zoom-sdk-android-5.11.3.7251\mobilertc-android-studio\mobilertc\mobilertc\res";
+    var regex = new Regex("%(s|d)|%([0-9]+d)");
+    //change your res folder to where you extracted the mobile rtc and then the common lib
     var resFolder =
-        @"C:\Users\adam_\Downloads\zoom-sdk-android-5.11.3.7251\zoom-sdk-android-5.11.3.7251\mobilertc-android-studio\commonlib\commonlib\res";
+        @"C:\Users\adam_\OneDrive\Business\Visual Service\Zoom Binding\5.11.3.7251\mobilertc\res";
+
+    //var resFolder = @"C:\Users\adam_\Downloads\test";
 
     var allFiles = Directory.GetFiles(resFolder, "**.xml", SearchOption.AllDirectories).Select(x => x.ToLowerInvariant()).Where(x => x.Contains("values"));
     foreach (var file in allFiles)
@@ -29,32 +48,19 @@ void Main()
         {
             for (var j = 0; j < allLines.Length; j++)
             {
-                var line = allLines[j];
-                
-                //todo for lines with both a d and an s, we will end up with %1$s %1$d when we want $1$s %2$d.
-                foreach (var placeholder in placeholders)
+                var matches = regex.Matches(allLines[j]);
+                if (matches.Any())
                 {
-                    var countOfPosition = Regex.Matches(line, $"%{placeholder}").Count;
-                    if (countOfPosition >= 1)
+                    Console.WriteLine($"Found problem in line: {allLines[j]}");
+                    var position = 1;
+                    while (matches.Any())
                     {
-                        Console.WriteLine($"Found problem in line: {line}");
-                        var newLine = line;
-                        var reconstructedLine = string.Empty;
-                        var allBlock = newLine.Split(new[] { $"%{placeholder}" }, StringSplitOptions.None);
-                        for (var i = 1; i <= allBlock.Length; i++)
-                        {
-                            if (i == allBlock.Length)
-                            {
-                                reconstructedLine += allBlock[i - 1];
-                            }
-                            else
-                            {
-                                reconstructedLine += allBlock[i - 1] + $"%{i}${placeholder}";
-                            }
-                        }
-                        allLines[j] = reconstructedLine;
-                        Console.WriteLine($"Reconstructed the line to: {reconstructedLine}");
+                        var letter = matches[0].Value.Contains("d") ? "d" : "s";
+                        allLines[j] =  regex.Replace(allLines[j], $"%{position}${letter}", 1);
+                        position++;
+                        matches =  regex.Matches(allLines[j]);
                     }
+                    Console.WriteLine($"Corrected line to: {allLines[j]}");
                 }
             }
             File.WriteAllLines(file, allLines);
