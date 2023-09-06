ac53bf0d3a50d6a7429ca211e9b63d97c1ec8b57
Adam Diament <adam@clevertree.io>
2023-09-06 10:33:14 +0300

    Use mobile buildtools for secrets

--- src/Xamarin/ZoomSDKSampleApp/ZoomSDKSampleApp/AppKeys.cs
+++ src/Xamarin/ZoomSDKSampleApp/ZoomSDKSampleApp/AppKeys.cs
@@ -1,12 +1,11 @@
-﻿using System;
-namespace ZoomSDKSampleApp
-{
-    public class AppKeys
-    {
-        public const string APP_KEY = "***REMOVED***";
-        public const string APP_SECRET = "***REMOVED***";
-        public const string A_MEETING_ID = "***REMOVED***";
-        public const string A_MEETING_PASSWORD = "***REMOVED***";
-    }
-}
+﻿using ZoomSDKSampleApp.Helpers;
 
+namespace ZoomSDKSampleApp;
+
+public class AppKeys
+{
+    public const string APP_KEY =  AppSettings.ZOOM_APP_KEY;
+    public const string APP_SECRET = AppSettings.ZOOM_APP_SECRET;
+    public const string A_MEETING_ID = AppSettings.ZOOM_MEETING_NUMBER;
+    public const string A_MEETING_PASSWORD = AppSettings.ZOOM_MEETING_PASSWORD;
+}
