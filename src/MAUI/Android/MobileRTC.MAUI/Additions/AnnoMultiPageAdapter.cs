fe248f0a727b029d3cbe0c9ac7e4fefb1e8500ef
Adam Diament <adam@clevertree.io>
2024-07-15 10:48:43 +0300

    Fix build errors

--- src/MAUI/Android/MobileRTC.MAUI/Additions/AnnoMultiPageAdapter.cs
+++ src/MAUI/Android/MobileRTC.MAUI/Additions/AnnoMultiPageAdapter.cs
@@ -2,6 +2,18 @@
 using System.Collections.Generic;
 using Android.Runtime;
 using Java.Interop;
+using Object = Java.Lang.Object;
+
+namespace US.Zoom.Zmsg.Dataflow
+{
+    public partial class MMFragmentModule : global::Java.Lang.Object, global::AndroidX.Core.Util.IConsumer
+    {
+        public void Accept(Object? t)
+        {
+            t?.NotifyAll();
+        }
+    }
+}
 
 namespace Com.Zipow.Annotate.AnnoMultiPage
 {
