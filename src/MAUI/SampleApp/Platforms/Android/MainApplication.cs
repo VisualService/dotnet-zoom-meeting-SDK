96d098ff546e1b3008978389b55f7afe62248f31
Adam Diament <adam@clevertree.io>
2024-07-16 22:09:31 +0300

    Almost there, 1 runtime error remaining

--- src/MAUI/SampleApp/Platforms/Android/MainApplication.cs
+++ src/MAUI/SampleApp/Platforms/Android/MainApplication.cs
@@ -2,16 +2,15 @@
 using Android.Runtime;
 using SampleApp.Platforms.Android;
 
-namespace SampleApp
-{
-    [Application]
-    public class MainApplication : MauiApplication
-    {
-        public MainApplication(IntPtr handle, JniHandleOwnership ownership)
-            : base(handle, ownership)
-        {
-        }
+namespace SampleApp;
 
-        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp(new DroidZoomSDKService());
+[Application]
+public class MainApplication : MauiApplication
+{
+    public MainApplication(IntPtr handle, JniHandleOwnership ownership)
+        : base(handle, ownership)
+    {
     }
+
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp(new DroidZoomSDKService());
 }
